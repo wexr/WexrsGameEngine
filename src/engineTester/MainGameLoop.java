4c7ea88ffb88cf6b14c841e33d6b7d26844c65e2
wexr <wexr.kk@gmail.com>
2018-02-16 13:09:18 +0100

    OpenGL 3D Game Tutorial 6: Texturing
    Made a res folder wherein pictures can be used for texture shading.
    shader and fragment updated to use textures.
    Loader updated to load VAO attribute list nr 1 (textures)
    Updated ShaderProgram to use textures instead of vertex colouring.
    Updated MainGameLoop to also use textures insated of vertex colouring.
    
    Vertex colour is still there. Its just being handled by textures.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,11 +1,13 @@
 package engineTester;
 
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -46,7 +48,16 @@ public class MainGameLoop {
 
         };
 
-        RawModel model = loader.loadToVAO(vertices, indices);
+        float[] textureCoords = {
+                0,0,            //V0
+                0,1,            //V1
+                1,1,            //V2
+                1,0             //V3
+        };
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("andras1"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
 
 
         while(!Display.isCloseRequested()) {
@@ -54,7 +65,7 @@ public class MainGameLoop {
             renderer.prepare();
             shader.start();
             //render
-            renderer.render(model);
+            renderer.render(texturedModel);
             shader.stop();
             DisplayManager.updateDisplay();
 
